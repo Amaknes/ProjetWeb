08a5e8929f1137e4852a8c651daeb0bf78a69d6e
Valhys <pvince2010@hotmail.fr>
2018-04-09 16:35:22 +0200

    pages login et register

--- BDE-CESI.fr/connexion.php
+++ BDE-CESI.fr/connexion.php
@@ -24,10 +24,36 @@
 	<header>
 		<?php include('header.php'); ?>
 	</header>
-	
-        <?php
-	
-        ?>
+            
+            
+            <section>
+				<div id="connexion">
+                    <div id="login" class="animate form">
+                        <form method="post" action="scriptConnexion.php" autocomplete="on">
+                            <h1>Connexion</h1> 
+                                <p> 
+                                    <label for="username" class="uname" data-icon="u" > Pseudo : </label>
+                                    <input id="username" name="pseudo" required="required" type="text" placeholder="pseudo"/>
+                                </p>
+                                <p> 
+                                    <label for="password" class="youpasswd" data-icon="p"> Mot de passe : </label>
+                                    <input id="password" name="motDePasse" required="required" type="password" placeholder="motdepasse" /> 
+                                </p>
+                            
+                                <p class="login button"> 
+                                    <input type="submit" value="Connexion" /> 
+                                </p>
+                                <p class="change_link">
+                                    Pas encore inscrit ?
+                                    <a href="inscription.php">Inscription</a>
+                                </p>
+                        </form>
+                    </div>                       
+                </div> 
+            </section>
+
+        
+    </div>
 
     </body>
 	
--- BDE-CESI.fr/inscription.php
+++ BDE-CESI.fr/inscription.php
@@ -24,24 +24,32 @@
 	<header>
 		<?php include('header.php'); ?>
 	</header>
-	
-        <?php
-			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
-
-			$Email = $_POST['Email'];
-			$Password = $_POST['Password'];
-			$requeteEmail = $bdd->prepare("SELECT * FROM users WHERE Email = ?");
-			$requeteEmail->execute(array($Email));
-			$test = $requeteEmail->fetch();
-			if(count($test) == 1){
-		// Requête préparée pour empêcher les injections SQL
-			$requete = $bdd->prepare("INSERT INTO users (Email, Password) VALUES( :Email,:Password)");
-			$requete->bindValue(':Email', $Email, PDO::PARAM_STR);
-			$requete->bindValue(':Password', $Password, PDO::PARAM_STR);
-
-			$requete->execute();
-			}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; 
-        ?>
+		
+		<section>
+			<div id="register" class="animate form">
+                <form  method="post" action="scriptInscription.php" autocomplete="on"> 
+                    <h1> Inscription </h1> 
+						<p> 
+                                    <label for="usernamesignup" class="uname" data-icon="u" >Pseudo : </label>
+                                    <input id="usernamesignup" name="pseudo" required="required" type="text" placeholder="pseudo" />
+						</p>
+                        
+						<p> 
+                                    <label for="passwordsignup" class="youpasswd" data-icon="p" >Mot de passe : </label>
+                                    <input id="passwordsignup" name="motDePasse" required="required" type="password" placeholder="mot de passe"/>
+						</p>
+                                
+						<p class="signin button"> 
+                                    <input type="submit" value="S'inscrire"/> 
+						</p>
+						<p class="change_link">  
+                                    Déjà inscrit ?
+                                    <a href="connexion.php">Connexion</a>
+						</p>
+                </form>
+            </div>
+						
+		</section>
 
     </body>
 	
