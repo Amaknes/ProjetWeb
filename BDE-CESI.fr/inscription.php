e4df5cd48e0f72d6845ebacb6bc9b959d29f9826
Valhys <pvince2010@hotmail.fr>
2018-04-09 16:01:18 +0200

    avec des tab c'est + joli

--- BDE-CESI.fr/inscription.php
+++ BDE-CESI.fr/inscription.php
@@ -26,21 +26,21 @@
 	</header>
 	
         <?php
-$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 
-$Email = $_POST['Email'];
-$Password = $_POST['Password'];
-$requeteEmail = $bdd->prepare("SELECT * FROM users WHERE Email = ?");
-$requeteEmail->execute(array($Email));
-$test = $requeteEmail->fetch();
-if(count($test) == 1){
-// Requête préparée pour empêcher les injections SQL
-$requete = $bdd->prepare("INSERT INTO users (Email, Password) VALUES( :Email,:Password)");
-$requete->bindValue(':Email', $Email, PDO::PARAM_STR);
-$requete->bindValue(':Password', $Password, PDO::PARAM_STR);
+			$Email = $_POST['Email'];
+			$Password = $_POST['Password'];
+			$requeteEmail = $bdd->prepare("SELECT * FROM users WHERE Email = ?");
+			$requeteEmail->execute(array($Email));
+			$test = $requeteEmail->fetch();
+			if(count($test) == 1){
+		// Requête préparée pour empêcher les injections SQL
+			$requete = $bdd->prepare("INSERT INTO users (Email, Password) VALUES( :Email,:Password)");
+			$requete->bindValue(':Email', $Email, PDO::PARAM_STR);
+			$requete->bindValue(':Password', $Password, PDO::PARAM_STR);
 
-$requete->execute();
-}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; 
+			$requete->execute();
+			}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; 
         ?>
 
     </body>
