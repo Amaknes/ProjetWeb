1fefbd6105bad4008a5cf1560272bc74cf2e30c6
Valhys <pvince2010@hotmail.fr>
2018-04-09 17:06:19 +0200

    lundisoir

--- BDE-CESI.fr/inscription.php
+++ BDE-CESI.fr/inscription.php
@@ -30,13 +30,21 @@
                 <form  method="post" action="scriptInscription.php" autocomplete="on"> 
                     <h1> Inscription </h1> 
 						<p> 
-                                    <label for="usernamesignup" class="uname" data-icon="u" >Pseudo : </label>
-                                    <input id="usernamesignup" name="pseudo" required="required" type="text" placeholder="pseudo" />
+                                    <label for="firstnamesignup" class="uname" data-icon="u" >Prénom : </label>
+                                    <input id="Prenom" name="Prenom" required="required" type="text" placeholder="Prenom" />
+						</p>
+						<p> 
+                                    <label for="lastnamesignup" class="uname" data-icon="u" >Nom : </label>
+                                    <input id="Nom" name="Nom" required="required" type="text" placeholder="Nom" />
+						</p>
+						<p> 
+                                    <label for="usernamesignup" class="uname" data-icon="u" >Email : </label>
+                                    <input id="Email" name="Email" required="required" type="text" placeholder="Email" />
 						</p>
                         
 						<p> 
                                     <label for="passwordsignup" class="youpasswd" data-icon="p" >Mot de passe : </label>
-                                    <input id="passwordsignup" name="motDePasse" required="required" type="password" placeholder="mot de passe"/>
+                                    <input id="Password" name="Password" required="required" type="password" placeholder="Password"/>
 						</p>
                                 
 						<p class="signin button"> 
--- BDE-CESI.fr/scriptInscription.php
+++ BDE-CESI.fr/scriptInscription.php
@@ -1,17 +1,25 @@
-        <?php
+<?php
 			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 
 			$Email = $_POST['Email'];
 			$Password = $_POST['Password'];
+			$FirstName = $_POST['Prenom'];
+			$LastName = $_POST['Nom'];
 			$requeteEmail = $bdd->prepare("SELECT * FROM users WHERE Email = ?");
 			$requeteEmail->execute(array($Email));
 			$test = $requeteEmail->fetch();
 			if(count($test) == 1){
 		// Requête préparée pour empêcher les injections SQL
-			$requete = $bdd->prepare("INSERT INTO users (Email, Password) VALUES( :Email,:Password)");
+			$requete = $bdd->prepare("INSERT INTO users (LastName, FirstName, Email, Password) VALUES( :Nom, :Prenom, :Email,:Password)");
+			$requete->bindValue(':Nom', $LastName, PDO::PARAM_STR);
+			$requete->bindValue(':Prenom', $FirstName, PDO::PARAM_STR);
 			$requete->bindValue(':Email', $Email, PDO::PARAM_STR);
 			$requete->bindValue(':Password', $Password, PDO::PARAM_STR);
 
-			$requete->execute();
+			if(!$requete->execute()){
+				print_r($requete->errorInfo());
+				
+			}
+			echo $Email;
 			}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; 
-        ?>
+?>
