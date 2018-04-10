046e74853952f6e2ffdb9feafd509bcff869c9c4
Valhys <pvince2010@hotmail.fr>
2018-04-10 17:31:22 +0200

    co deco ok

--- BDE-CESI.fr/deconnexion.php
+++ BDE-CESI.fr/deconnexion.php
@@ -6,6 +6,7 @@
  Contexte : Projet WEB Exia CESI Saint-Nazaire
  #######################################-->
  <?php
+	session_start();
 	unset($_SESSION['Nom']);
 	unset($_SESSION['Prenom']);
 	echo '<meta http-equiv="refresh" content="0;URL=accueil.php">' 
