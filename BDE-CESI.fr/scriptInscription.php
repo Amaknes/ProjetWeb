25a50bd919d8ff4ed614fa26657aac7c7cde49e4
Valhys <pvince2010@hotmail.fr>
2018-04-10 11:01:53 +0200

    scriptco

--- BDE-CESI.fr/scriptInscription.php
+++ BDE-CESI.fr/scriptInscription.php
@@ -10,16 +10,29 @@
 			$test = $requeteEmail->fetch();
 			if(count($test) == 1){
 		// Requête préparée pour empêcher les injections SQL
+					if (preg_match("#[A-Za-z0-9]#", $Password)) {
+						echo 'Mot de passe conforme';
+ 
+ 
+ 
 			$requete = $bdd->prepare("INSERT INTO users (LastName, FirstName, Email, Password) VALUES( :Nom, :Prenom, :Email,:Password)");
 			$requete->bindValue(':Nom', $LastName, PDO::PARAM_STR);
 			$requete->bindValue(':Prenom', $FirstName, PDO::PARAM_STR);
 			$requete->bindValue(':Email', $Email, PDO::PARAM_STR);
 			$requete->bindValue(':Password', $Password, PDO::PARAM_STR);
-
-			if(!$requete->execute()){
+						}
+  
+ 
+					else {
+ 
+						echo 'Mot de passe non conforme';
+ 
+						} 
+			/*if(!$requete->execute()){
 				print_r($requete->errorInfo());
 				
 			}
 			echo $Email;
-			}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; 
+			}else echo "<h1>WHY IS THE FBI HERE ?!</h1>"; --->*/
+			}
 ?>
