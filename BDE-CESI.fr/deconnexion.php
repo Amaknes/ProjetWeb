694fce88e4e20d63d8163c55cef26f505138cbe7
Amaknes <amaury.vincent@viacesi.fr>
2018-04-10 17:28:27 +0200

    Branchement Déconnexion

--- BDE-CESI.fr/deconnexion.php
+++ BDE-CESI.fr/deconnexion.php
@@ -5,54 +5,8 @@
  Date : 2018
  Contexte : Projet WEB Exia CESI Saint-Nazaire
  #######################################-->
- 
-<html>
-<?php session_start(); ?>
-	<header>
-		<?php include('header.php'); ?>
-	</header>
-	
-	
-    <head>
-        <title>BDE CESI EXIA St Nazaire</title>
-    </head>
-	
-	
-    <body>
-            
-            <section>
-				<div id="connexion">
-                    <div id="login" class="animate form">
-                        <form method="post" action="scriptConnexion.php" autocomplete="on">
-                            <h1>Connexion</h1>
-                                <p> 
-                                    <label for="Email" class="uname" data-icon="u" > Email : </label>
-                                    <input id="Email" name="Email" required="required" type="text" placeholder="Email"/>
-                                </p>
-                                <p> 
-                                    <label for="Password" class="youpasswd" data-icon="p"> Mot de passe : </label>
-                                    <input id="Password" name="Password" required="required" type="password" placeholder="Password" /> 
-                                </p>
-                            
-                                <p class="login button"> 
-                                    <input type="submit" value="Connexion" /> 
-                                </p>
-                                <p class="change_link">
-                                    Pas encore inscrit ?
-                                    <a href="inscription.php">Inscription</a>
-                                </p>
-                        </form>
-                    </div>                       
-                </div> 
-            </section>
-
-        
-    </div>
-
-    </body>
-	
-	<footer>
-		<?php include('footer.php'); ?>
-	</footer>
-	
-</html>
+ <?php
+	unset($_SESSION['Nom']);
+	unset($_SESSION['Prenom']);
+	echo '<meta http-equiv="refresh" content="0;URL=accueil.php">' 
+ ?>
