0acafdc8e7721a43bff82de0d999ecab17ae30eb
Valhys <pvince2010@hotmail.fr>
2018-04-11 11:03:25 +0200

    session

--- BDE-CESI.fr/header.php
+++ BDE-CESI.fr/header.php
@@ -5,13 +5,26 @@
 		<link rel="stylesheet" href="style.css" />
 		
 		<img src="Ressources/Images/Site_Banner.png">
-			
+	
+		<div>
+		<?php
+		if(isset($_SESSION['Nom'])){
+			echo ($_SESSION['Prenom']." ".$_SESSION['Nom']);
+			echo "<a href='deconnexion.php'>Déconnexion</a>";
+		}else{
+			echo "<a href='inscription.php'>Inscription</a>";
+			echo "<a href='connexion.php'>Connexion</a>";
+		}
+		?>
+		</div>
+		
 		<nav>
 		
 			<?php include('nav.php');?>
 			<br>
 
 		</nav>
+
 	
     </header>
  
--- BDE-CESI.fr/nav.php
+++ BDE-CESI.fr/nav.php
@@ -3,16 +3,7 @@
     <nav>
 		<a href="accueil.php">Accueil</a>
 		<a href="evenements.php">Événements</a>
-		<a href="boutique.php">Goodies</a>
-		<?php
-		if(isset($_SESSION['Nom'])){
-			echo ("Bonjour".$_SESSION['Prenom']." ".$_SESSION['Nom']);
-			echo "<a href='deconnexion.php'>Déconnexion</a>";
-		}else{
-			echo "<a href='inscription.php'>Inscription</a>";
-			echo "<a href='connexion.php'>Connexion</a>";
-		}
-		?>
+		<a href="boutique.php">Boutique</a>
 	</nav>
  
 </html>
