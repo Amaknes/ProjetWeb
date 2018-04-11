90257d2068838a087aab8e44335c12dae65b7401
Amaknes <amaury.vincent@viacesi.fr>
2018-04-11 16:17:03 +0200

    Fix

--- BDE-CESI.fr/DisplayEvenement.php
+++ BDE-CESI.fr/DisplayEvenement.php
@@ -1,3 +1,13 @@
 <?php
 $bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+$requete = $bdd->prepare("SELECT * FROM Events WHERE Selected = true");
+$requete = execute();
+$ans = $requete->fetch();
+if(count($ans) == 1){
+		echo "<h1>PAS OK</h1>";
+	}else{  
+	echo "<p class='EventTitle'>".$ans[1]."</p>";
+	echo "<img src="$ans[4]">"
+	echo "<p class='EventText'>".$ans[5]."</p>";
+	}
 ?>
