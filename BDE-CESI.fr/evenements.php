99451eb68dc7359402b6892758f37403324494f6
Amaknes <amaury.vincent@viacesi.fr>
2018-04-11 16:39:40 +0200

    refactor variables et fichier inutile

--- BDE-CESI.fr/evenements.php
+++ BDE-CESI.fr/evenements.php
@@ -22,7 +22,17 @@
     <body>
 		<h3>Évènement du mois</h3>
         <?php
-	
+			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+			$requete = $bdd->prepare("SELECT * FROM Events WHERE Selected = true");
+			$requete = execute();
+			$ans = $requete->fetch();
+			if(count($ans) == 1){
+				echo "<h1>PAS OK</h1>";
+			}else{  
+				echo "<p class='EventTitle'>".$ans[1]."</p>";
+				echo '<img src="'.$ans[4].'"/>'
+				echo "<p class='EventText'>".$ans[5]."</p>";
+			}
         ?>
 		<h3>Liste des évènements</h3>
     </body>
