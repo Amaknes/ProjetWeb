dfe6c31af51200a104a9320eb467be22a63b03eb
Amaknes <amaury.vincent@viacesi.fr>
2018-04-12 10:12:59 +0200

    Création nouveaux fichiers pour les événements

---
+++ BDE-CESI.fr/EvenementListe.php
@@ -0,0 +1,6 @@
+<!DOCTYPE HTML>
+<!--####################################
+ Auteur : Groupe 3 (Moyon Matthis, Pasquet Vincent, Chéraud Florentin, Amaury Vincent)
+ Date : 2018
+ Contexte : Projet WEB Exia CESI Saint-Nazaire
+ #######################################-->
---
+++ BDE-CESI.fr/EvenementUnique.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML>
+<!--####################################
+ Auteur : Groupe 3 (Moyon Matthis, Pasquet Vincent, Chéraud Florentin, Amaury Vincent)
+ Date : 2018
+ Contexte : Projet WEB Exia CESI Saint-Nazaire
+ #######################################-->
+ 
