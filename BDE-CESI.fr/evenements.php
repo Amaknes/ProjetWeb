6336041f828e95a8cae76206140f62ab1f0c025c
Valhys <pvince2010@hotmail.fr>
2018-04-12 11:09:30 +0200

    BISMILLAH

--- BDE-CESI.fr/evenements.php
+++ BDE-CESI.fr/evenements.php
@@ -24,7 +24,7 @@
 		<?php
 			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 			$requete = $bdd->prepare("SELECT * FROM Events WHERE Selected = true");
-			$requete = execute();
+			$requete->execute();
 			$ans = $requete->fetch();
 			if(count($ans) == 1){
 				echo "<h1>PAS OK</h1>";
