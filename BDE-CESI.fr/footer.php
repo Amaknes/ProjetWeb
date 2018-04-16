35888a7a417c894898624038f8e82ca1299af371
Valhys <pvince2010@hotmail.fr>
2018-04-16 17:18:54 +0200

    css

--- BDE-CESI.fr/footer.php
+++ BDE-CESI.fr/footer.php
@@ -3,33 +3,34 @@
 				<a href="accueil.php"> <img class="RetourAccueil" img src="Ressources/SiteImages/BDE_CESI_logo_no_text.png" alt="RetourAccueil"</a>
 			</div>
 			
-			<div id="TexteRetourAccueil">
-				<a href="accueil.php">BDE CESI Saint-Nazaire</a>
-			</div>
+			<div id="content">
+				<div id="TexteRetourAccueil">
+					<a href="accueil.php">BDE CESI Saint-Nazaire</a>
+				</div>
 			
 			
-			<div id="contact">
+				<div id="contact">
 				
-				<div id="external-link">
-					<a class="extern fb" target="_blank" href="https://www.facebook.com" title="Facebook"></a>
-				</div>
-				<div id="external-link">
-					<a class="extern twitter" target="_blank" href="https://twitter.com" title="Twitter"></a>
-				</div>
-				<div id="external-link">
-					<a class="extern linkedin" target="_blank" href="https://fr.linkedin.com" title="LinkedIn"></a>
-				</div>
-				<div id="external-link">
-					<a class="extern youtube" target="_blank" href="https://www.youtube.com/?hl=fr&gl=FR" title="youtube"></a>
-				</div>
-				<div id="external-link">
-					<a class="extern instagram" target="_blank" href="https://instagram.com" title="Instagram"></a>
-				</div>
-				<div id="external-link">
-					<a class="extern mail" target="_blank" href="#" title="Mail"></a>
+					<div id="external-link">
+						<a class="extern fb" target="_blank" href="https://www.facebook.com" title="Facebook"></a>
+					</div>
+					<div id="external-link">
+						<a class="extern twitter" target="_blank" href="https://twitter.com" title="Twitter"></a>
+					</div>
+					<div id="external-link">
+						<a class="extern linkedin" target="_blank" href="https://fr.linkedin.com" title="LinkedIn"></a>
+					</div>
+					<div id="external-link">
+						<a class="extern youtube" target="_blank" href="https://www.youtube.com/?hl=fr&gl=FR" title="youtube"></a>
+					</div>
+					<div id="external-link">
+						<a class="extern instagram" target="_blank" href="https://instagram.com" title="Instagram"></a>
+					</div>
+					<div id="external-link">
+						<a class="extern mail" target="_blank" href="#" title="Mail"></a>
+					</div>
 				</div>
 			</div>
-			
 
 			<div id="MentionsLegales">
 				<a class="Mentionslegales" target="_blank" href="mentionslegales.php" title="MentionsLegales" >Mentions Légales</a> 
