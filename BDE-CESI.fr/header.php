5d372942a1f7d3eb6286ebcbe121a53294b4e032
Valhys <pvince2010@hotmail.fr>
2018-04-17 09:57:05 +0200

    footer header css

--- BDE-CESI.fr/header.php
+++ BDE-CESI.fr/header.php
@@ -8,24 +8,37 @@
     </head>
 	<body>
     <header>
-		<img src="Ressources/SiteImages/Site_Banner.png">
-		<div>
+		<div class="session">
 		<?php
 		if(isset($_SESSION['Nom'])){
 			echo ($_SESSION['Prenom']." ".$_SESSION['Nom']);
 			echo "<a href='deconnexion.php'>Déconnexion</a>";
 		}else{
-			echo "<a href='inscription.php'>Inscription</a>";
+			echo "<a class='test' href='inscription.php'>Inscription</a>";
 			echo "<a href='connexion.php'>Connexion</a>";
 		}
 		?>
 		</div>
 		
+ 	
+	<div id="LogoBBE">
+		<a href="accueil.php"> <img class="LogoBDE" img src="Ressources/SiteImages/BDE_CESI_logo_no_text.png" alt="LogoBDE"></a>
+	</div>
+	<nav>
+		<a class="accueilnav" href="accueil.php">Accueil</a>
+		<a class="evenementsnav" href="evenements.php">Événements</a>
+		<a class="boutiquenav" href="boutique.php">Boutique</a>
+	</nav>
+	<div id="BDEHeader">
+		<a href="accueil.php">BDE CESI Saint-Nazaire</a>
+	</div>
+	
 
-		
-			<?php include('nav.php');?>
-			<br>
-
+	
+    <div id="banniere">	
+			<p> Site</p>
+	</div>
+	
 	
     </header>
 	
