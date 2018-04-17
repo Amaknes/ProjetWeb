068b307c40121cb4a7e8f1df8e178149562763bc
Amaknes <amaury.vincent@viacesi.fr>
2018-04-17 10:55:10 +0200

    Ajout backbone EvenementListe

--- BDE-CESI.fr/EvenementListe.php
+++ BDE-CESI.fr/EvenementListe.php
@@ -3,4 +3,24 @@
  Auteur : Groupe 3 (Moyon Matthis, Pasquet Vincent, Chéraud Florentin, Amaury Vincent)
  Date : 2018
  Contexte : Projet WEB Exia CESI Saint-Nazaire
- #######################################-->
+ #######################################-->	
+ 
+ <html>
+	<?php session_start(); ?>
+	<header>
+		<?php include('header.php'); ?>
+	</header>
+	
+	
+    <head>
+        <title>BDE CESI EXIA St Nazaire</title>
+		<h2>Événements</h2>
+    </head>
+	
+	<body>
+	</body>
+	
+		<footer>
+		<?php include('footer.php'); ?>
+	</footer>
+</html>
