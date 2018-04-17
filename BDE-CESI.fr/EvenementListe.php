1b18b39a322b5d74eba26db79ffca6ebc541b60a
Amaknes <amaury.vincent@viacesi.fr>
2018-04-17 11:30:34 +0200

    Ajout Listes Evenement

--- BDE-CESI.fr/EvenementListe.php
+++ BDE-CESI.fr/EvenementListe.php
@@ -14,13 +14,25 @@
 	
     <head>
         <title>BDE CESI EXIA St Nazaire</title>
-		<h2>Événements</h2>
+		<h2>Liste des Événements</h2>
     </head>
 	
 	<body>
+		<?php
+			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+			$requete = $bdd->prepare("SELECT * FROM Events");
+			$requete->execute();
+			foreach($requete as $ans){
+				echo '<div href ="EvenementUnique.php?id='.$ans[0].'">';
+					echo "<p class='EventTitle'>".$ans[1]."</p>";
+					echo '<img class="EventThumbnail" src="'.$ans[4].'"/>';
+					echo "<p class='EventText'>".$ans[5]."</p>";
+				echo "</div>";
+			}
+		?>
 	</body>
 	
-		<footer>
+	<footer>
 		<?php include('footer.php'); ?>
 	</footer>
 </html>
