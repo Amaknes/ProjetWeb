5c20568cf9f388f7911d775d0731a541cf1342b3
Valhys <pvince2010@hotmail.fr>
2018-04-18 11:26:17 +0200

    t

---
+++ BDE-CESI.fr/Panier.php
@@ -0,0 +1,18 @@
+<?php session_start(); ?>
+		<?php include('header.php'); ?>
+		
+<!--####################################
+ Auteur : Groupe 3 (Moyon Matthis, Pasquet Vincent, Chéraud Florentin, Amaury Vincent)
+ Date : 2018
+ Contexte : Projet WEB Exia CESI Saint-Nazaire
+ #######################################-->
+
+ 
+ 
+<content id="Panier">
+	<div id="banniere">	
+		<h2>Mon panier</h2>
+	</div>
+</content>
+
+		<?php include('footer.php'); ?>
