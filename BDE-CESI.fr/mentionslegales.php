125a941726556005f3ae367fc209bc8dfe3f4314
Valhys <pvince2010@hotmail.fr>
2018-04-19 00:51:35 +0200

    fn

--- BDE-CESI.fr/mentionslegales.php
+++ BDE-CESI.fr/mentionslegales.php
@@ -10,7 +10,35 @@
 	<content id="MentionsLegales">
 		<div id="banniere">	
 			<h2>Mentions Légales</h2>
-		</div>	
+		</div>
+		
+		<p>MENTIONS LÉGALES
+Ce site a été réalisé par des élèves de l’exia.CESI dans le cadre d’un projet scolaire de deuxième année. 
+ÉDITION DU SITE
+Matthis MOYON, Vincent PASQUET, Florentin CHÉRAUD et Amaury VINCENT. 
+Exia.CESI Campus Gavy 44600 Saint Nazaire
+bdesaintnazaire@viacesi.fr
+HÉBERGEMENT DU SITE
+Site hébergé par 1and1
+Adresse postale : 1&1 Internet SARL 7, place de la Gare BP 70109 57201 Sarreguemines Cedex
+Tel : 0970 808 911 (appel non surtaxé)
+Fax : 03 87 95 99 72
+Mail : support@1and1.fr
+Site : www.1and1.fr
+DROITS D’AUTEURS
+L'ensemble de ce site relève de la législation française et internationale sur le droit d'auteur et la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents téléchargeables et les représentations iconographiques et photographiques.
+COOKIES
+Lors de la consultation de notre site, des cookies sont déposés sur votre ordinateur, votre mobile ou votre tablette.
+Pour plus d’information sur les cookies : https://www.cnil.fr/fr/cookies-les-outils-pour-les-maitriser 
+MODIFICATION DU SITE
+L’équipe éditoriale se réserve le droit de modifier ou de corriger le contenu de ce site et de ces mentions légales à tout moment et ceci sans préavis. 
+FONCTIONNEMENT
+En cas de disfonctionnement d’une partie du site, veuillez nous notifier à l’adresse suivante : webmaster@bdesaintnazaire.fr</p>
+
+		
+		
+		
+		
 	</content>
 	
 		<?php include('footer.php'); ?>
