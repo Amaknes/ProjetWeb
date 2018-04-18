b45652087eb03681c9d452cf114e9aa58c4224d7
FlorentinC <cheraud.florentin@gmail.com>
2018-04-18 14:53:43 +0200

    zaet

--- BDE-CESI.fr/BoiteAIdees.php
+++ BDE-CESI.fr/BoiteAIdees.php
@@ -1,5 +1,8 @@
 
 	<?php session_start(); ?>
+	<head>
+	<link rel="stylesheet" href="css/BoiteAIdees.css"/>
+	</head>
 	<?php include('header.php'); ?>
 	
 
@@ -12,41 +15,45 @@
 	
 <content id="BoiteAIdees">
 	<div id="banniere">	
-		<h2>Boite a idées</h2>
+		<h2>Boite à idées</h2>
 	</div>	
 
 	
-	
-
-		<h3>Proposer une activités</h3>
-<?php if(isset($_SESSION['Status'])){} else{echo "<p>Vous devez être connecté pour pouvoir participer ou proposer une idée</p>"; } ?>
-		
-						<p> 
-                                    <label for="Activity" class="Activity">Activité</label>
-                                    <input id="Activity" name="Activity" required="required" type="Activity" placeholder="Décrivez votre activité"/>
-						</p>
-                                
-						<p class="Confirm button"> 
-                                    <input type="submit" value="Proposer"/> 
-						</p>
-		<h3>Voter pour une idées</h3>
-		<?php
-			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
-			$requete = $bdd->prepare("SELECT Activity,LastName,FirstName FROM Ideas INNER JOIN Users ON Users.IDUser = Ideas.IDUser WHERE IdeaFlag=false");
-			$requete->execute();
-			foreach($requete as $ans){
-				echo '<div>';
-					echo "<p class='IdeeName'>".$ans[2]." ".$ans[1]."</p>";
-					echo "<p class='IdeeContent'>".$ans[0]."</p>";
-					if(isset($_SESSION['Status'])){ 
-						echo "<button>Voter pour cette idée</button>";
-						if($_SESSION['Status']==2) echo "<button>Signaler comme inapproprié</button>";
-						if($_SESSION['Status']==3) echo "<button>Créer un événement</button>";
-					}
-				echo "</div>";
-			}
-		?>
-
+	<section id="sectionidees">
+		<div class="proposition">
+			<h3>Proposer une activités</h3>
+				<?php if(isset($_SESSION['Status'])){} else{echo "<p>Vous devez être connecté pour pouvoir participer ou proposer une idée</p>"; } ?>
+			
+							<p> 
+										<label for="Activity" class="Activity">Activité</label>
+										<br/>
+										<textarea id="Activity" name="Activity" required="required" type="text" placeholder="Décrivez votre activité"/>
+							</p>
+									
+							<p class="Confirm button"> 
+										<input type="submit" value="Proposer"/> 
+							</p>
+		</div>
+		<div class="ideavote">
+			<h3>Voter pour une idées</h3>
+			<?php
+				$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+				$requete = $bdd->prepare("SELECT Activity,LastName,FirstName FROM Ideas INNER JOIN Users ON Users.IDUser = Ideas.IDUser WHERE IdeaFlag=false");
+				$requete->execute();
+				foreach($requete as $ans){
+					echo '<div>';
+						echo "<p class='IdeeName'>".$ans[2]." ".$ans[1]."</p>";
+						echo "<p class='IdeeContent'>".$ans[0]."</p>";
+						if(isset($_SESSION['Status'])){ 
+							echo "<button>Voter pour cette idée</button>";
+							if($_SESSION['Status']==2) echo "<button>Signaler comme inapproprié</button>";
+							if($_SESSION['Status']==3) echo "<button>Créer un événement</button>";
+						}
+					echo "</div>";
+				}
+			?>
+		</div>
+	</section>
 </content>
 
 	<?php include('footer.php'); ?>
