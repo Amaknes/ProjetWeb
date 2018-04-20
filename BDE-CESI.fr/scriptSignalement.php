a6caa9713581fb09dc31171a6c7ed19fa5c45f10
Amaknes <amaury.vincent@viacesi.fr>
2018-04-20 07:14:20 +0200

    Commentaires Signalement

--- BDE-CESI.fr/scriptSignalement.php
+++ BDE-CESI.fr/scriptSignalement.php
@@ -3,10 +3,12 @@ $Typereq = $_GET['type'];
 $Idreq = $_GET['id'];
 $bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 
+//Vérification du type d'élément passé en GET
 switch($Typereq){
 	
+	//Si c'est une image
 	case "Pic" :
-	
+		//Requête d'ajout du Flag
 		$requete = $bdd->prepare("UPDATE Pictures SET PicFlag = 1 WHERE IDPicture = ?");
 		$requete->bindValue(1, $Idreq, PDO::PARAM_INT);
 		$requete->execute();
@@ -16,14 +18,15 @@ switch($Typereq){
 		$requete2->bindValue(1, $Idreq, PDO::PARAM_INT);
 		$requete2->execute();
 		$ans = $requete2->fetch();
-	
+		//Redirection vers l'événement précédent
 		echo ("<meta http-equiv='refresh' content='0;URL=EvenementUnique.php?id=");
 		echo ($ans[0]);
 		echo ("'>");
 	
 	break;
+	//Si c'est un commentaire
 	case "Comment" :
-		
+		//Requête d'ajout du Flag
 		$var_dump($Idreq);
 		$requete = $bdd->prepare("UPDATE Comments SET CommentFlag = 1 WHERE IDComment = ?");
 		$requete->bindValue(1, $Idreq, PDO::PARAM_INT);
@@ -34,14 +37,15 @@ switch($Typereq){
 		$requete2->bindValue(1, $Idreq, PDO::PARAM_INT);
 		$requete2->execute();
 		$ans = $requete2->fetch();
-	
+		//Redirection vers l'événement précédent
 		echo ("<meta http-equiv='refresh' content='0;URL=EvenementUnique.php?id=");
 		echo ($ans[0]);
 		echo ("'>");
 	
 	break;
+	//Si c'est une idée
 	case "Idea" :
-	
+		//Requête d'ajout du Flag
 		$requete = $bdd->prepare("UPDATE Ideas SET IdeaFlag = 1 WHERE IDIdea = ?");
 		$requete->bindValue(1, $Idreq, PDO::PARAM_INT);
 		$requete->execute();
