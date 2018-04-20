4e52b13f04a28847496d21e1a94e70a3fc5f4dd7
Amaknes <amaury.vincent@viacesi.fr>
2018-04-20 07:22:50 +0200

    Commentairesv1

--- BDE-CESI.fr/scriptPostImage.php
+++ BDE-CESI.fr/scriptPostImage.php
@@ -1,22 +1,22 @@
 <?php
 session_start();
+//Récupération des paramètres
 $IDParent= $_POST['IDEvent']; //A faire, utiliser un attribut readonly dans le form
 $URL = $_POST['URLImage'];
 $email = $_SESSION['Email'];
-
+//Requête pour retrouver l'utilisateur courant
 $bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 $requete = $bdd->prepare("SELECT IDUser FROM Users WHERE Email = ?");
 		$requete->bindValue(1, $email, PDO::PARAM_STR);
 		$requete->execute();
 		$ans = $requete->fetch();
-		
+//requête d'insertion d'image	
 $requete2 = $bdd->prepare("INSERT INTO Pictures (URLImage, PicFlag, IDEvent, IDUser) VALUES (:Url, 0, :Event, :User)");
 		$requete2->bindValue(':Url', $URL_FINAL , PDO::PARAM_STR);
 		$requete2->bindValue(':Event', $IDParent, PDO::PARAM_INT);
 		$requete2->bindValue(':User', $ans[0], PDO::PARAM_INT);
 		$requete2->execute();
-		
-
+//redirection
 echo '<meta http-equiv="refresh" content="0;URL=EvenementListe.php">'
 
 ?>
--- BDE-CESI.fr/scriptVote.php
+++ BDE-CESI.fr/scriptVote.php
@@ -4,7 +4,7 @@
 	$IDIdea = $_GET['idea'];
 	$email = $_SESSION['Email'];
 
-	var_dump($IDIdea);
+	//Requête récupération de l'utilisateur courant
 
 	$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
 	$requete = $bdd->prepare("SELECT IDUser FROM Users WHERE Email = ?");
@@ -12,7 +12,8 @@
 		$requete->bindValue(1, $email, PDO::PARAM_STR);
 		$requete->execute();
 		$ans = $requete->fetch();
-		
+	
+	//Requête insertion du vote	
 	$requete2 = $bdd->prepare("INSERT INTO Vote (IDUser, IDIdea) VALUES( :IDUser, :IDIdea) ");
 		$requete2->bindValue(':IDUser', $ans[0], PDO::PARAM_STR);
 		$requete2->bindValue(':IDIdea', $IDIdea, PDO::PARAM_STR);
