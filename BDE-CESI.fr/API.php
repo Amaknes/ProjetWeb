6bd63d17149b34361fedf77f6d9e1ff80befb513
Amaknes <amaury.vincent@viacesi.fr>
2018-07-12 02:56:34 +0200

    API Final

--- BDE-CESI.fr/API.php
+++ BDE-CESI.fr/API.php
@@ -1,36 +1,74 @@
 <?php
+include 'scriptNouveauProduit.php';
+
 function Create_APIUser($Username,$Password){
 	$PasswordCrypted = openssl_encrypt($Password,'aes128','R75CxT0wcYb+oNSuml9nMcocrJUpqAkLEPwN8OJrwE4=');
 	setcookie($Username , $PasswordCrypted,time() + (86400 * 365), "/"); //Cookie qui dure un an
 }
 
 function LoginToAPI($Username,$Password){
-  if($Password == openssl_encrypt($_COOKIE[$Username],'aes128','R75CxT0wcYb+oNSuml9nMcocrJUpqAkLEPwN8OJrwE4=')){
-	return TRUE;	
+  if($Password == openssl_decrypt($_COOKIE[$Username],'aes128','R75CxT0wcYb+oNSuml9nMcocrJUpqAkLEPwN8OJrwE4=')){
+	return true;	
   }else{
-	return FALSE;
+	return false;
   }
  }
 
  switch($_SERVER['REQUEST_METHOD']){
- case'GET':
- header("HTTP/1.0 405 Methode GET non acceptee");
- break;
+	case'GET':
+	header("HTTP/1.0 405 Methode GET non acceptee");
+	break;
  
- case'PUT':
- header("HTTP/1.0 405 Methode PUT non acceptee");
- break;
+	case'PUT':
+	header("HTTP/1.0 405 Methode PUT non acceptee");
+	break;
  
- case'POST':
+	case'POST':
+	$JSON_DATA = json_decode(file_get_contents('php://input'),TRUE);
+	if(isset($JSON_DATA["Request"]) && isset($JSON_DATA["Username"]) && isset($JSON_DATA["Password"])){
+	
+		switch($JSON_DATA["Request"]){
+			
+			case "AccessRequest":
+				Create_APIUser($JSON_DATA["Username"],$JSON_DATA["Password"]);
+				header("HTTP/1.0 200 Acces cree");
+			break;
+			
+			case "CreateProduct":
+				LoginToAPI($JSON_DATA["Username"],$JSON_DATA["Password"]);
+				if(isset($JSON_DATA["ProductName"]) && isset($JSON_DATA["Category"]) && isset($JSON_DATA["Price"]) && isset($JSON_DATA["URLImage"]))
+				CreateNewproduct($JSON_DATA["ProductName"],$JSON_DATA["Category"],$JSON_DATA["Price"],$JSON_DATA["URLImage"])
+				header("HTTP/1.0 200 Produit Cree");
+				}else{
+				header("HTTP/1.0 422 Requete invalide");	
+				}
+			break;
+		
+			case "TestLogin" :
+				if(LoginToAPI($JSON_DATA["Username"],$JSON_DATA["Password"])){
+				header("HTTP/1.0 200 Connexion Effectuee Correctement");
+				}else{
+				header("HTTP/1.0 401 Mauvais Identifiant ou Mot de Passe");
+				}
+			break;
+			
+			default:
+				header("HTTP/1.0 422 Requete invalide");
+				break;
+			}
+	
+	}else{
+		header("HTTP/1.0 401 Vous devez vous authentifier ou creer un acces");
+	} 
  
- break;
+	break;
  
- case 'DELETE':
- header("HTTP/1.0 405 Methode DELETE non acceptee");
- break;
+	case 'DELETE':
+	header("HTTP/1.0 405 Methode DELETE non acceptee");
+	break;
  
- default:
- header("HTTP/1.0 405 Methode non acceptée");
- break;
+	default:
+	header("HTTP/1.0 405 Methode non acceptée");
+	break;
 }
 ?>
--- BDE-CESI.fr/scriptNouveauProduit.php
+++ BDE-CESI.fr/scriptNouveauProduit.php
@@ -1,22 +1,15 @@
 ﻿<?php
-			session_start();
-			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
-
-			$Name = $_POST['Name'];
-			$Category = $_POST['Category'];
-			$Price = $_POST['Price'];
-			$UrlImage = $_POST['UrlImage'];
-
+			function CreateNewproduct($NameF,$CategoryF,$PriceF,$UrlImageF){
 			$requeteName = $bdd->prepare("SELECT * FROM products WHERE Name = ?");
-			$requeteName->execute(array($Name));
+			$requeteName->execute(array($NameF));
 			$test = $requeteName->fetch();
 			if(count($test) == 1){
 			
 									$requete = $bdd->prepare("INSERT INTO products (Name, Category, Price, UrlImage) VALUES( :Name, :Category, :Price,:UrlImage)");
-									$requete->bindValue(':Name', $Name, PDO::PARAM_STR);
-									$requete->bindValue(':Category', $Category, PDO::PARAM_STR);
-									$requete->bindValue(':Price', $Price, PDO::PARAM_STR);
-									$requete->bindValue(':UrlImage', $UrlImage, PDO::PARAM_STR);
+									$requete->bindValue(':Name', $NameF, PDO::PARAM_STR);
+									$requete->bindValue(':Category', $CategoryF, PDO::PARAM_STR);
+									$requete->bindValue(':Price', $PriceF, PDO::PARAM_STR);
+									$requete->bindValue(':UrlImage', $UrlImageF, PDO::PARAM_STR);
 					
 									$requete->execute();
 									
@@ -30,5 +23,15 @@
 				echo"</script>";
 				echo '<meta http-equiv="refresh" content="0;URL=NouveauProduit.php">';
 			} 	
+			}
 			
+			session_start();
+			$bdd = new PDO('mysql:host=localhost; dbname=projetweb; charset=utf8', 'root', '');
+
+			$Name = $_POST['Name'];
+			$Category = $_POST['Category'];
+			$Price = $_POST['Price'];
+			$UrlImage = $_POST['UrlImage'];
+			
+			CreateNewproduct($Name, $Category, $Price, $UrlImage);
 ?>
